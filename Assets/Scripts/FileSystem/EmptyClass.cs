d735eb2ca67236bf6914031cb529eff86a68c1d2
George <georgealeks@hotmail.com>
2017-07-22 16:31:25 -0500

    WIP Filesystem

---
+++ Assets/Scripts/FileSystem/EmptyClass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+[TestFixture]
+public class FileSystemTest {
+	
+	[Test]
+	public void InstantiateProperly() {
+		File f = new File ("test", "txt");
+		Assert.IsNotNull (f);
+		Assert.AreEqual (f.getName(), "test.txt");
+		Assert.AreEqual (f.getContents (), "");
+	}
+
+	[Test]
+	public void CanSetContents() {
+		File f = new File ("test", "txt");
+	}
+}
