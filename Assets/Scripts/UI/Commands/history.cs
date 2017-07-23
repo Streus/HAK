40b971e57ecfc77a40df55718201b291f0900d4b
George <georgealeks@hotmail.com>
2017-07-23 16:10:41 -0500

    Fixed history

--- Assets/Scripts/UI/Commands/history.cs
+++ Assets/Scripts/UI/Commands/history.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace Commands
 {
-	public class cd : Command
+	public class history : Command
 	{
 		public override string getHelp ()
 		{
@@ -18,13 +19,25 @@ namespace Commands
 
 		public override string execute (params string[] args)
 		{
-			int count = args [1];
-			string[] history = Console.getHistory ();
+			int count = 10;
+			if (args.Length > 1) {
+				try {
+					count = Int32.Parse(args [1]);
+				} catch (Exception e) {
+					e.ToString ();
+					throw new ExecutionException ("Couldn't parse integer: " + args [1]);
+				}
+			}
 
+			string[] history = Console.log.getHistory ();
 			if (count > 0) {
+				if (history.Length == 0) {
+					return "\nNo history found.";
+				}
+
 				string toReturn = "";
-				for (int i = 0; i < Mathf.Min(count, history.Length); i++) {
-					toReturn += history [i];
+				for (int i = Mathf.Min(count, history.Length) - 1; i >= 0; i--) {
+					toReturn += "\n[" + (history.Length - i) + "] " + history [i];
 				}
 				return toReturn;
 			} else {
