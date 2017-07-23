bdff81c623781f2e939830841c7f8d3684f789f4
George <georgealeks@hotmail.com>
2017-07-23 15:55:07 -0500

    Added history command.

---
+++ Assets/Scripts/UI/Commands/history.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Commands
+{
+	public class cd : Command
+	{
+		public override string getHelp ()
+		{
+			return "Get <count> amount of lines of history. Usage: history <count=10>.";
+		}
+
+		public override string getInvocation ()
+		{
+			return "history";
+		}
+
+		public override string execute (params string[] args)
+		{
+			int count = args [1];
+			string[] history = Console.getHistory ();
+
+			if (count > 0) {
+				string toReturn = "";
+				for (int i = 0; i < Mathf.Min(count, history.Length); i++) {
+					toReturn += history [i];
+				}
+				return toReturn;
+			} else {
+				throw new ExecutionException ("Count needs to be a non-negative integer.");
+			}
+		}
+	}
+}
